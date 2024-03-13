7df38d9900d452e47ffee3146da65902841927af
Sangwon Yang / 양상원 <120084331+sangwonsheep@users.noreply.github.com>
2024-03-13 19:15:57 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.03 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -6,35 +6,36 @@ class Solution {
     
     public int[] solution(String[][] places) {
         int[] answer = new int[places.length];
-        for (int i = 0; i < places.length; i++) {
+        
+        for(int i = 0; i < places.length; i++) {
             char[][] place = new char[5][5];
-            for (int j = 0; j < 5; j++) {
+            for(int j = 0; j < 5; j++) {
                 place[j] = places[i][j].toCharArray();
             }
-            answer[i] = isSafe(place) ? 1 : 0;
+            answer[i] = check(place) ? 1 : 0;
         }
         return answer;
     }
     
-    public boolean isSafe(char[][] place) {
-    
-    for (int x = 0; x < 5; ++x) {
-        for (int y = 0; y < 5; ++y) {
-            if (place[x][y] == 'P') {
-                for (int i = 0; i < 4; ++i) {
-                    int nx = x + dx[i];
-                    int ny = y + dy[i];
-                    if (nx < 0 || ny < 0 || nx >= 5 || ny >= 5 || place[nx][ny] == 'X')
-                        continue;
-                    
-                    if (place[nx][ny] == 'B' || place[nx][ny] == 'P')
-                        return false;
+    public boolean check(char[][] place) {
+        for(int x = 0; x < 5; x++){
+            for(int y = 0; y < 5; y++){
+                if(place[x][y] == 'P'){
+                    for(int i = 0; i < 4; i++) {
+                        int nx = x + dx[i];
+                        int ny = y + dy[i];
+                        
+                        if(nx < 0 || ny < 0 || nx >= 5 || ny >= 5 || place[nx][ny] == 'X')
+                            continue;
 
-                    place[nx][ny] = 'B';
+                        if(place[nx][ny] == 'B' || place[nx][ny] == 'P')
+                            return false;
+
+                        place[nx][ny] = 'B';
+                    }
                 }
             }
         }
+        return true;
     }
-    return true;
-}
 }

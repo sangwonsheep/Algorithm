6f4c04d1d54da33710d1ed268cd1851f7c6f3642
Sangwon Yang / 양상원 <120084331+sangwonsheep@users.noreply.github.com>
2024-03-15 01:38:55 +0900

    [level 2] Title: 우박수열 정적분, Time: 8.28 ms, Memory: 86.8 MB -BaekjoonHub

--- 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.java
+++ 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.java
@@ -6,30 +6,29 @@ class Solution {
         List<Integer> list = new ArrayList<>();
         getCollatz(list, k);
         
-        for(int i = 0; i < ranges.length; i++){
+        int n = list.size() - 1;
+        
+        for(int i = 0; i < ranges.length; i++) {
+            int start = ranges[i][0];
+            int end = n + ranges[i][1];
             
-            if(ranges[i][0] > ranges[i][1] + list.size() - 1){
+            if(start > end) {
                 answer[i] = -1;
                 continue;
-            }else if(ranges[i][0] == ranges[i][1] + list.size() - 1){
-                answer[i] = 0;
-                continue;
             }
             
-            double dimension = 0;
-
-            for(int j = ranges[i][0]; j < ranges[i][1] + list.size() - 1; j++){
-                dimension += (list.get(j) + list.get(j + 1)) / 2.0;
-            }
+            double sum = 0;
             
-            answer[i] = dimension;
+            for(int j = start; j < end; j++) {
+               sum += (list.get(j) + list.get(j+1)) / 2.0;
+            }
+            answer[i] = sum;
         }
-        
+    
         return answer;
     }
     
     public void getCollatz(List<Integer> list, int k) {
-        
         while(k != 1){
             list.add(k);
             if(k % 2 == 0) 

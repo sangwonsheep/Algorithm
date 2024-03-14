16648473ffd8a00d3385a301ca974b8094947b5d
Sangwon Yang / 양상원 <120084331+sangwonsheep@users.noreply.github.com>
2024-03-14 12:47:57 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.49 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
@@ -1,5 +1,9 @@
 import java.util.*;
 
+/*
+<테스트 케이스 2번을 실패하시는 분들>
+테스트 케이스 2번은 cards = [ 2, 3, 4, 5, 6, 7, 8, 9, 10 , 1 ] 와 같은 첫 시도에 10개의 모든 상자를 연 경우, 두 번째 시도는 모든 상자가 열려 있기 때문에 '0'이 되므로 '0'을 반환해야 합니다.
+*/
 class Solution {
     public int solution(int[] cards) {
         List<Integer> list = new ArrayList<>();
@@ -11,10 +15,10 @@ class Solution {
                 continue;
 
             int count = 0;
+            
             cards[i] = 0;
-            while(true) {
-                if(card == 0)
-                    break;
+            
+            while(card != 0) {
                 count++;
                 next = cards[card-1];
                 cards[card-1] = 0;
@@ -32,5 +36,25 @@ class Solution {
         return list.get(0) * list.get(1);
     }
     
-
+    // 다른 풀이
+    private int solveByLoof(int[] cards) {
+        List<Integer> answer = new ArrayList<>();
+        boolean[] visited = new boolean[cards.length];
+        for(int card: cards) {
+            int count = 0;
+            card = card - 1;
+            while(!visited[card]) {
+                visited[card] = true;
+                count++;
+                card = cards[card] - 1;
+            }
+            answer.add(count);
+        }
+        if(answer.size() == 1) {
+            return 0;
+        }
+        Collections.sort(answer, Comparator.reverseOrder());
+        return answer.get(0) * answer.get(1);
+    }
+    
 }

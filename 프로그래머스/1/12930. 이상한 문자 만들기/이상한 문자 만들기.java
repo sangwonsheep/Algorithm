8f12a16580a44cc01f41963441f2d4db87984b37
Sangwon Yang / 양상원 <swonyy1709@naver.com>
2024-04-05 03:28:23 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.11 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,17 +1,22 @@
 class Solution {
     public String solution(String s) {
-        String[] str = s.split(" ", -1);
         StringBuilder sb = new StringBuilder();
-        for(int x = 0; x < str.length; x++) {
-            String word = str[x];
-            for(int i = 0; i < word.length(); i++) {
-                if(i % 2 == 0)
-                    sb.append(Character.toUpperCase(word.charAt(i)));
-                else
-                    sb.append(Character.toLowerCase(word.charAt(i)));
+        boolean check = true;
+        
+        for(char c : s.toCharArray()) {
+            if(!Character.isAlphabetic(c)) {
+                sb.append(c);
+                check = true;
             }
-            if(x < str.length-1)
-                sb.append(" ");
+            else {
+                if(check){
+                    sb.append(Character.toUpperCase(c));
+                }
+                else{
+                    sb.append(Character.toLowerCase(c));
+                }
+                check = !check;    
+            }
         }
         return sb.toString();
     }

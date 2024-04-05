955492e352f25d7c3706f858a7676c571180f5c1
Sangwon Yang / 양상원 <swonyy1709@naver.com>
2024-04-05 20:41:14 +0900

    [level 2] Title: 문자열 압축, Time: 12.16 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        int answer = Integer.MAX_VALUE;
+        
+        for(int i = 0; i < s.length()-1; i++) {
+            StringBuilder sb = new StringBuilder();
+            List<String> split = new ArrayList();
+            for(int j = 0; j < s.length(); j += (i+1)) {
+                int lastIndex = (j+i+1);
+                if(lastIndex > s.length()){
+                    lastIndex = s.length();
+                }
+                split.add(s.substring(j, lastIndex));
+            }
+            
+            // System.out.println("배열 체크 : " + split.get(split.size()-1));
+            
+            int count = 1;
+            for(int j = 0; j < split.size()-1; j++) {
+                if(split.get(j).equals(split.get(j+1))){
+                    count++;
+                    if(j == split.size()-2){
+                        sb.append(count + split.get(j));
+                    }
+                }
+                else {
+                    if(count > 1){
+                        sb.append(count + split.get(j));
+                        if(j == split.size()-2) {
+                            sb.append(split.get(j+1));
+                        }
+                    }
+                    else {
+                        sb.append(split.get(j));
+                        if(j == split.size()-2) {
+                            sb.append(split.get(j+1));
+                        }
+                    }
+                    count = 1;
+                }
+            }
+            
+            answer = Math.min(answer, sb.length());
+            // System.out.println(sb.toString());
+        }
+        if(s.length() == 1)
+            return 1;
+        return answer;
+    }
+}

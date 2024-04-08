a5b08311927efb50618efb6d87661e75052fba1c
Sangwon Yang / 양상원 <swonyy1709@naver.com>
2024-04-08 19:58:01 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.05 ms, Memory: 65.7 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,10 +1,9 @@
 class Solution {
     public int solution(String s) {
-        String[] word = new String[]{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        String[] words = new String[]{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
         
-        for(int i = 0; i < word.length; i++){
-            if(s.contains(word[i]))
-               s = s.replace(word[i], ""+ i);
+        for(int i = 0; i < words.length; i++){
+            s = s.replace(words[i], Integer.toString(i));
         }
             
         return Integer.parseInt(s);

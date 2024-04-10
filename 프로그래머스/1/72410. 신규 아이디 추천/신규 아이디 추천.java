3ad4038264d92700fb4ce309d60a670118372db1
Sangwon Yang / 양상원 <swonyy1709@naver.com>
2024-04-11 03:34:42 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.25 ms, Memory: 84.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String solution(String new_id) {
+        String answer = new_id.toLowerCase(); // 1
+        answer = answer.replaceAll("[^a-z0-9\\-_.]", ""); // 2
+        answer = answer.replaceAll("\\.+", "."); // 3
+        answer = answer.replaceAll("^\\.|\\.$", ""); // 4
+        
+        if(answer.isEmpty()) {
+            answer += "a"; // 5
+        }
+        
+        if(answer.length() >= 16) {
+            answer = answer.substring(0, 15);
+            answer = answer.replaceAll("\\.$", ""); // 6
+        }
+        
+        while(answer.length() < 3) {
+            answer += answer.charAt(answer.length()-1);
+        }
+        
+        return answer;
+    }
+}

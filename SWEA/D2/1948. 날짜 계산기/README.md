3ce18b0d8f4494dcee45bb19f37ccd9bf4e4febc
Sangwon Yang / 양상원 <swonyy1709@naver.com>
2024-04-25 04:25:02 +0900

    [D2] Title: 날짜 계산기, Time: 133 ms, Memory: 26,468 KB -BaekjoonHub

---
+++ SWEA/D2/1948. 날짜 계산기/README.md
@@ -0,0 +1,15 @@
+# [D2] 날짜 계산기 - 1948 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5PnnU6AOsDFAUq) 
+
+### 성능 요약
+
+메모리: 26,468 KB, 시간: 133 ms, 코드길이: 906 Bytes
+
+### 제출 일자
+
+2024-04-25 04:24
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D2/1948. 날짜 계산기/날짜 계산기.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int t = input.nextInt();
+
+        // 우, 하, 좌, 상
+        int[] month_day = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        for(int tc = 1; tc <= t; tc++) {
+            int m1 = input.nextInt();
+            int d1 = input.nextInt();
+            int m2 = input.nextInt();
+            int d2 = input.nextInt();
+
+            int result = 1;
+            if(m1 == m2) {
+                result += d2-d1;
+            }
+            else {
+                result += month_day[m1] - d1;
+                result += d2;
+                for(int i = m1+1; i <= m2-1; i++) {
+                    result += month_day[i];
+                }
+            }
+            System.out.println("#" + tc + " " + result);
+        }
+    }
+}

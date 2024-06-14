104ee02d1404fa8163a7c7579d6598212b8a7f83
Sangwon Yang / 양상원 <sangwonsheep@gmail.com>
2024-06-14 15:34:49 +0900

    [level 1] Title: K번째수, Time: 0.37 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -4,14 +4,15 @@ class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         
-        for(int i = 0; i < commands.length; i++) {
-            int[] sub = new int[commands[i][1]-commands[i][0]+1];
-            int index = 0;
-            for(int j = commands[i][0]-1; j < commands[i][1]; j++) {
-                sub[index++] = array[j];
-            }
+        for(int i = 0; i < answer.length; i++) {
+            int[] command = commands[i];
+            int from = command[0]-1;
+            int to = command[1];
+            int k = command[2]-1;
+            
+            int[] sub = Arrays.copyOfRange(array, from, to);
             Arrays.sort(sub);
-            answer[i] = sub[commands[i][2]-1];
+            answer[i] = sub[k];
         }
         
         return answer;

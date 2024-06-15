34324e5ea4ccb52d399f6fbf4373798bf382c652
Sangwon Yang / 양상원 <sangwonsheep@gmail.com>
2024-06-15 22:59:12 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 2460 ms, Memory: 188968 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        int[][] points = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 2; j++) {
+                points[i][j] = input.nextInt();
+            }
+        }
+
+        Arrays.sort(points, (i1, i2) -> {
+            if(i1[0] == i2[0])
+                return i1[1]-i2[1];
+            return i1[0]-i2[0];
+        });
+
+        for (int[] point : points) {
+            System.out.println(point[0] + " " + point[1]);
+        }
+    }
+}

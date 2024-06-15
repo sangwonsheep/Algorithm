0f0bf7a79b3bcfa781336eb2ca16e7d32fb2101e
Sangwon Yang / 양상원 <sangwonsheep@gmail.com>
2024-06-15 18:40:45 +0900

    [Silver V] Title: 영화감독 숌, Time: 364 ms, Memory: 159720 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+
+        int num = 666;
+        int count = 1;
+
+        while (count != n) {
+            num++;
+            if(String.valueOf(num).contains("666")){
+                count++;
+            }
+        }
+        System.out.println(num);
+    }
+}

50de080e1ad406f226efb87112392d227609452e
Sangwon Yang / 양상원 <sangwonsheep@gmail.com>
2024-06-17 22:54:30 +0900

    [Bronze IV] Title: $A + B - C$, Time: 80 ms, Memory: 11464 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+        
+        String s = "" + a + b;
+        
+        System.out.println(a+b-c);
+        System.out.println(Integer.parseInt(s) - c);
+    }
+}

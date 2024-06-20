79a2acb64b51e075a7c24afc463be244626599ef
Sangwon Yang / 양상원 <sangwonsheep@gmail.com>
2024-06-21 02:42:09 +0900

    [Silver II] Title: 연속합, Time: 216 ms, Memory: 33120 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[] mem;
+    static int max = -1001;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+        StringTokenizer st = new StringTokenizer(s);
+
+        mem = new int[n];
+        Arrays.fill(mem, -1);
+
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = Integer.parseInt(st.nextToken());
+        }
+
+        mem[0] = array[0];
+        max = array[0];
+
+        recur(n - 1, array);
+        System.out.println(max);
+    }
+
+    private static int recur(int n, int[] array) {
+        if(n < 0)
+            return 0;
+
+        if(mem[n] != -1)
+            return mem[n];
+
+        mem[n] = Math.max(recur(n-1, array) + array[n], array[n]);
+        max = Math.max(mem[n], max);
+        return mem[n];
+    }
+
+}

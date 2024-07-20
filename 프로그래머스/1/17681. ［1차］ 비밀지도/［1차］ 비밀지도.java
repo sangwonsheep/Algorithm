6df4c250effa1f1f92fedcc0055a18a9054ce27f
Sangwon Yang / 양상원 <sangwonsheep@gmail.com>
2024-07-20 13:39:01 +0900

    [level 1] Title: [1차] 비밀지도, Time: 8.04 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        
+        for(int i = 0; i < n; i++) {
+            String s = Integer.toBinaryString(arr1[i] | arr2[i]);
+            s = String.format("%" + n + "s", s);
+            s = s.replace("1", "#");
+            s = s.replace("0", " ");
+            answer[i] = s;
+        }
+        
+        return answer;
+    }
+}

29514ee8eb693858079f902a84b2fd1f73ccfe58
Sangwon Yang <sangwonsheep@gmail.com>
2025-04-05 02:43:57 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.04 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -2,7 +2,6 @@ class Solution {
     public int solution(int n, int w, int num) { // n : 총 박스 수, w : 한 층에 필요한 박스, num : 꺼내기
         int answer = 0;
         int nextNumber = num;
-        int floor = num % w == 0 ? num / w - 1 : num / w;
         while (nextNumber <= n) {
             nextNumber += nextNumber % w == 0 ? 1 : (w - (nextNumber % w)) * 2 + 1;
             answer++;

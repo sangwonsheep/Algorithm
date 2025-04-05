17ff54a3721c23fdeab3e3f5e106bf9e51374a84
Sangwon Yang <sangwonsheep@gmail.com>
2025-04-05 18:09:02 +0900

    [level 1] Title: 유연근무제, Time: 51.58 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int solution(int[] schedules, int[][] timelogs, int startday) {
+        int answer = 0;
+        for(int i = 0; i < schedules.length; i++) {
+            boolean isLate = false;
+            int day = startday;
+            for (int j = 0; j < 7; j++) {
+                if (day > 7) {
+                    day = 1;
+                }
+                if (day != 6 && day != 7) {
+                    int schedule = schedules[i] + 10;
+                    if (schedule % 100 >= 60) {
+                        schedule += (100 - 60); // 시 자릿수 올리기
+                    }
+                    int time = timelogs[i][j];
+                    System.out.println(schedule + " " + time);
+                    
+                    if (schedule < time) {
+                        isLate = true;
+                        break;
+                    }
+                }
+                day++;
+            }
+            if (!isLate) {
+                answer++;
+            }
+        }            
+        return answer;
+    }
+}

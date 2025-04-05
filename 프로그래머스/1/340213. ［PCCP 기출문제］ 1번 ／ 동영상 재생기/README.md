9440571aa1d35ecb8a7d4f64e6755fbcc2add5d5
Sangwon Yang <sangwonsheep@gmail.com>
2025-04-05 20:44:08 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 15.86 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/README.md
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/README.md
@@ -1,10 +1,10 @@
 # [level 1] [PCCP 기출문제] 1번 / 동영상 재생기 - 340213 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340213) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340213#) 
 
 ### 성능 요약
 
-메모리: 92.8 MB, 시간: 17.89 ms
+메모리: 78.2 MB, 시간: 15.86 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 05일 20:38:07
+2025년 04월 05일 20:44:06
 
 ### 문제 설명
 
--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -2,73 +2,43 @@ import java.util.*;
 
 class Solution {
     public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
-        // 동영상 길이
-        StringTokenizer st = new StringTokenizer(video_len, ":");
-        int videoMinute = Integer.parseInt(st.nextToken());
-        int videoSecond = Integer.parseInt(st.nextToken());
-        int video = minuteToSecond(videoMinute, videoSecond);
-        
-        // 동영상 재생 위치
-        st = new StringTokenizer(pos, ":");
-        int startMinute = Integer.parseInt(st.nextToken());
-        int startSecond = Integer.parseInt(st.nextToken());
-        int start = minuteToSecond(startMinute, startSecond);
-        
-        // 오프닝 시작 시간
-        st = new StringTokenizer(op_start, ":");
-        int opStartMinute = Integer.parseInt(st.nextToken());
-        int opStartSecond = Integer.parseInt(st.nextToken());
-        int opStart = minuteToSecond(opStartMinute, opStartSecond);
-        
-        // 오프닝 종료 시간
-        st = new StringTokenizer(op_end, ":");
-        int opEndMinute = Integer.parseInt(st.nextToken());
-        int opEndSecond = Integer.parseInt(st.nextToken());
-        int opEnd = minuteToSecond(opEndMinute, opEndSecond);
+        int video = minuteToSecond(new StringTokenizer(video_len, ":"));
+        int start = minuteToSecond(new StringTokenizer(pos, ":"));
+        int opStart = minuteToSecond(new StringTokenizer(op_start, ":"));
+        int opEnd = minuteToSecond(new StringTokenizer(op_end, ":"));
         
         if (opStart <= start && start <= opEnd) {
-            startMinute = opEndMinute;
-            startSecond = opEndSecond;
+            start = opEnd;
         }
         
         for (int i = 0; i < commands.length; i++) {
             if (commands[i].equals("next")) {
-                startSecond += 10;
-                if (startSecond >= 60) {
-                    startMinute += 1;
-                    startSecond -= 60;
-                }
-                start = minuteToSecond(startMinute, startSecond);
+                start += 10;
                 if (start > video) {
-                    startMinute = videoMinute;
-                    startSecond = videoSecond;
+                    start = video;
                 }
-
             }
             else {
-                startSecond -= 10;
-                if (startSecond < 0) {
-                    startMinute -= 1;
-                    startSecond += 60;
-                }
-                if (startMinute < 0) {
-                    startMinute = 0;
-                    startSecond = 0;
+                start -= 10;
+                if (start < 0) {
+                    start = 0;
                 }
             }
-            
-            start = minuteToSecond(startMinute, startSecond);
-           
+                       
             if (opStart <= start && start <= opEnd) {
-                startMinute = opEndMinute;
-                startSecond = opEndSecond;
+                start = opEnd;
             }
         }
         
-        return String.format("%02d", startMinute) + ":" + String.format("%02d", startSecond);
+        int minute = start / 60;
+        int second = start % 60;
+        
+        return String.format("%02d", minute) + ":" + String.format("%02d", second);
     }
                 
-    public int minuteToSecond(int minute, int second) {
+    public int minuteToSecond(StringTokenizer st) {
+        int minute = Integer.parseInt(st.nextToken());
+        int second = Integer.parseInt(st.nextToken());
         return minute * 60 + second;
     }
 
